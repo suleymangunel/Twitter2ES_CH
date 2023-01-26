7683dc3070376647e6b3a4ecdbcb29aef511b7ec
sgunel <116437234+sgunel@users.noreply.github.com>
2023-01-26 14:43:43 +0300

    Modules updated.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import LogicUnit as LGu
 
 if __name__ == '__main__':
     # LGu.collect_tweets()
-    results_es = LGu.get_top_tweets(3)
+    results_es = LGu.get_top_tweets(3)  # TOP 3 tweets by numerical value: twitter.retweetCount+twitter.likeCount
     for _res in results_es:
         print(_res['_source']['content'])
 
